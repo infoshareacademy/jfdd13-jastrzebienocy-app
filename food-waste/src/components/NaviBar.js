2eb755ede080b76b0c18d2da96ca122d2d93ce81
Filip Siwiec <filipsiwiec@gmal.com>
2019-11-15 16:36:45 +0100

    adding Navibar

---
+++ food-waste/src/components/NaviBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./NaviBar.module.css";
+
+
+class NaviBar extends React.Component {
+    <nav className={styles.NaviBar}>
+    <a href="">logo</a>
+    <a href="">Dashboard</a>
+    <a href="">Przepisy</a>
+    <a href="">Progil</a>
+</nav>
+}
+
+
+export default NaviBar;
