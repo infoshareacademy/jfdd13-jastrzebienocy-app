8ff902014f29e393c949645686595c2e18337dcc
Filip Siwiec <filipsiwiec@gmal.com>
2019-11-16 13:10:49 +0100

    navi bar/ responsive not done yet

--- food-waste/src/components/NaviBar.js
+++ food-waste/src/components/NaviBar.js
@@ -1,16 +1,21 @@
 import React from "react";
 import styles from "./NaviBar.module.css";
-
+import Logo from "./logo-nav.png";
 
 class NaviBar extends React.Component {
-    render () { return (
-    <div className={styles.NaviBar}>
-        <a href="" className={styles.logo}>Logo</a>
-        <a href="" className={styles.Dashboard}>Dashboard</a>
-        <a href="" className={styles.Przepisy}>Przepisy</a>
-        <a href="" className={styles.Profil}>Profil</a>
-    </div>
-    )
+    render() {
+        return (
+            <div className={styles.NaviBar}>
+                    <span>
+                    <img src={Logo} className={styles.logo}/>
+                    </span>
+             <div className={styles.NaviBarRight}>
+                    <a href="" className={styles.Dashboard}>Dashboard</a>
+                    <a href="" className={styles.Przepisy}>Przepisy</a>
+                    <a href="" className={styles.Profil}>Profil</a>
+                    </div> 
+            </div>
+        )
     }
 }
 
