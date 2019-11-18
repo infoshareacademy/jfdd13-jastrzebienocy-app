7ff65249fef7a0cfae99e4e7ce410e9192ee1254
Filip Siwiec <filipsiwiec@gmal.com>
2019-11-18 15:24:21 +0100

    Footer almost finished

--- food-waste/src/components/Footer.js
+++ food-waste/src/components/Footer.js
@@ -4,14 +4,12 @@ import styles from "./Footer.module.css";
 class Footer extends React.Component {
     render() {
         return (
-            <div className={styles.Footer}>Jakis ladny Footer</div>
-
-    //      <div className={styles.footerBox, styles.footerBoxMobile}>
-    //   <span className={styles.copywright}>&copy 2019 Food Waste</span>
-    //   <a className={styles.footerLinks} href="">Kontakt</a></span>
-    //   <a className={styles.footerLinks} href="">Polityka prywatności</a></span>
-    //     </div>
-         
+         <div className={`${styles.footerBox} ${styles.footerBoxMobile}`}>
+         <span className={styles.copywright}>© 2019 Food Waste</span>
+          <a className={styles.footerLinks} href="">Kontakt</a>
+          <a className={styles.footerLinks} href="">Polityka prywatności</a>     
+         </div>
+    
         )
     }
 }
