b846d85c7f5e67a3b38e68101212b17fa80c0c53
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-11-19 18:47:29 +0100

    Initial of serch inputt in project

--- food-waste/src/components/Search.js
+++ food-waste/src/components/Search.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import styles from './Se';
+import styles from './Search.module.css';
+import Recipes from '../Recipes';
 
+// const
+// Search input by products
 class Search extends React.Component {
-	constructor() {
-		super();
-	}
+	state = {
+		// recipes: Recipes,
+		filteringProducts: [ products ]
+	};
 
+	filterResults = () => {};
+
+	// Rendering of input
 	render() {
+		const { filterResults } = this.state;
 		return (
 			<div>
-				<input className="{styles.searchElement}" type="text" />
+				<input className={styles.searchElement} type="text" />
 			</div>
 		);
 	}
