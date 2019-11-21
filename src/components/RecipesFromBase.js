0dd3334079246fb8b8440f318d4dcc4ab19807f9
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-11-21 16:45:38 +0100

    Fixed Problems

--- src/components/RecipesFromBase.js
+++ src/components/RecipesFromBase.js
@@ -1,68 +1,49 @@
-import React from 'react';
-import RecipeView from './RecipeView';
+import React from 'react'
+import RecipeView from './RecipeView'
 
-<<<<<<< HEAD:food-waste/src/components/RecipesFromBase.js
-import { Grid } from 'semantic-ui-react';
-=======
-import {Grid} from 'semantic-ui-react';
-import SideBar from './SideBar';
-
->>>>>>> master:src/components/RecipesFromBase.js
+import { Grid } from 'semantic-ui-react'
+import SideBar from './SideBar'
 
 export class RecipesFromBase extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor (props) {
+    super(props)
 
-		this.state = {
-			recipes: []
-		};
-	}
-
-	componentDidMount() {
-		fetch('https://foodwaste-ecb78.firebaseio.com/recipes.json')
-			.then((res) => res.json())
-			// Zamienione przez JK
-			.then((object) =>
-				Object.keys(object).map((key) => {
-					object[key].id = key;
-					return object[key];
-				})
-			)
-			// .then((obj) => Object.values(obj))
-			.then((allRecipes) => this.setState({ recipes: allRecipes }))
-			//Dodane JK
-			.then((data) => console.log(this.state.recipes));
-	}
-
-<<<<<<< HEAD:food-waste/src/components/RecipesFromBase.js
-	render() {
-		return (
-			// Dodane JK div na razie
-			<div>
-				<Grid>
-					{this.state.recipes.map((item) => (
-						<Grid.Column width={8}>
-							<RecipeView recipe={item} />
-						</Grid.Column>
-					))}
-				</Grid>
-			</div>
-		);
-	}
-}
-=======
-    render() {
-        return (
-        <div style={{display: 'flex'}}>
-             <div style={{background: 'grey', marginRight: '20px'}}><SideBar/></div>
-        
-        <Grid>
-            { this.state.recipes.map( item =>
-            <Grid.Column width={8} style={{}}><RecipeView recipe={item} /></Grid.Column>
-            )}
-            </Grid>
-            </div>)
-            
+    this.state = {
+      recipes: []
     }
+  }
+
+  componentDidMount () {
+    fetch('https://foodwaste-ecb78.firebaseio.com/recipes.json')
+      .then(res => res.json())
+      // Zamienione przez JK
+      .then(object =>
+        Object.keys(object).map(key => {
+          object[key].id = key
+          return object[key]
+        })
+      )
+      // .then((obj) => Object.values(obj))
+      .then(allRecipes => this.setState({ recipes: allRecipes }))
+      // Dodane JK
+      .then(data => console.log(this.state.recipes))
+  }
+
+  render () {
+    return (
+      <div style={{ display: 'flex' }}>
+        <div style={{ background: 'grey', marginRight: '20px' }}>
+          <SideBar />
+        </div>
+
+        <Grid>
+          {this.state.recipes.map(item => (
+            <Grid.Column width={8} style={{}}>
+              <RecipeView recipe={item} />
+            </Grid.Column>
+          ))}
+        </Grid>
+      </div>
+    )
+  }
 }
->>>>>>> master:src/components/RecipesFromBase.js
