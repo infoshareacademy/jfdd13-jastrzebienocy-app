c015969c1016cc37ff8b06b2c53b1aed5cc58615
Filip Siwiec <filipsiwiec@gmal.com>
2019-11-23 20:58:28 +0100

    footer fixed/Profile page in progress

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,21 +1,27 @@
 import React from "react";
 import styles from "./Profile.module.css";
 import ProfilePicture from "./images/ProfilePicture.jpg";
+import { Icon } from 'semantic-ui-react';
+import Heart from './Heart';
 
 class Profile extends React.Component {
     render() {
         return (
-            <div className={styles.ProfileMain}>
-                <img src={ProfilePicture} ClassName={styles.ProfilePicture} alt={'Profile picture'}></img>
-                <div className={styles.Name}>
-                    Gerwazy Zawyszyński
+            <div>
+                <div className={styles.ProfileMain}>
+                    <div className={styles.ProfileFlex}>
+                        <img src={ProfilePicture} ClassName={styles.ProfilePicture} alt={'Profile picture'}></img>
+                        <div className={styles.ProfileRight}>
+                            <div className={styles.Name}>Gerwazy Zawyszyński</div>
+                            <div className={styles.Email}>fluidmonster@wp.pl</div>
+                        </div>
+                    </div>
+                    <div className={styles.FavoutiteRecipe}>Ulubione przepisy "icon:heart"
+                    </div>
                 </div>
-                <div className={styles.Email}>fluidmonster@wp.pl</div>
-                <div className={styles.FavoutiteRecipe}>Ulubione przepisy : </div>
             </div>
-
         )
     }
-};
+}
 
 export default Profile;
