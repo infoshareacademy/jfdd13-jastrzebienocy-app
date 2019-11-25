f64328255680587456e650520828b8a4f1b3655f
Kinga Zawarczynska <zawarczynska.ko@gmail.com>
2019-11-25 10:39:06 +0100

    changed display of modalWindow

--- src/components/ModalWindow.js
+++ src/components/ModalWindow.js
@@ -4,47 +4,41 @@ import { Modal, Image, Button, Segment } from "semantic-ui-react";
 
 class ModalWindow extends React.Component {
   constructor(props){
-    super(props);
-    // this.closeWindow = closeWindow()
-    
-    
-}
-  
-  onClick() {  
-  
-   
-
+    super(props);  
 }
 
   render() {
    
     return (
     <Modal open={this.props.open} >
-      <Segment styles={{display: 'flex'}}>
+      <h1>{this.props.name}</h1>
+      <Segment style={{display: 'flex', flexDirection: 'column'}}>
       
-        <h1>{this.props.name}</h1>
+        
 
-         
-              <Image
+              <div><Image
                 src={this.props.imageUrl}
                 style={{
                   width: 200,
                   height: 200,
                   objectFit: "cover",
-                  objectPosition: "center"
+                  objectPosition: "center",
+                  margin: "0 40% 2% 40%",
+                  circular: false
+                 
+                  
                 }}
                 size="medium"
                 floated="left"
               />
+              </div>
             
             <div >{this.props.description}</div>
             </Segment>
-              
-          
-
             <Modal.Actions>
                     <Button negative onClick={() => this.props.onCloseWindow()}>Zamknij</Button>
                     </Modal.Actions>
+                    
             
     </Modal>);
   }
