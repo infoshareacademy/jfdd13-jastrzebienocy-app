17e6a36b9a6cbfbac86a44e90a15c0a07a67f28e
Filip Siwiec <filipsiwiec@gmal.com>
2019-12-03 19:57:10 +0100

    styling registration form

--- src/components/email/RegisterForm.js
+++ src/components/email/RegisterForm.js
@@ -12,32 +12,33 @@ export default class RegisterForm extends React.Component {
         err: ''
     };
 
-     getMessage (code) {
-         console.log(code)
-         switch (code) {
-             case "auth/email-already-in-use": 
+    getMessage(code) {
+        console.log(code)
+        switch (code) {
+            case "auth/email-already-in-use":
                 return "Email już jest przypisany!";
-             case "auth/invalid-email":
-                return "Niepoprawny Emeil"
-             case "auth/weak-password":
-                 return "Twoje hasło musi posiadać przynajmniej 6 znaków"
-             default: 
+            case "auth/invalid-email":
+                return "Niepoprawny Email"
+            case "auth/weak-password":
+                return "Twoje hasło musi posiadać przynajmniej 6 znaków"
+            default:
                 return "Wystąpił nieoczekiwany błąd"
-         }
-     }
-     
+        }
+    }
+
+    // validation
+
+    // validation (type, password) {
+
+    // }
 
     onSubmit = e => {
         // console.log(api)
-
         e.preventDefault();
         api.register(this.state.email, this.state.password, this.state.name)
             .catch(err => this.setState({ err: this.getMessage(err.code) }));
         // this.props.apiMethod(this.state.email, this.state.password, this.state.name)
         //     .catch(err => this.setState({ err: err.message }));
-        
-
-
     }
 
     render() {
@@ -45,31 +46,32 @@ export default class RegisterForm extends React.Component {
             <div>
                 <div className={styles.Register}>Utwórz konto</div>
                 <div className={styles.InnerBox}>
-                <p className={styles.MailPar}>Proszę wypełnić formularz w celu rejestracji.</p>
-                <form className={styles.Inputs} onSubmit={this.onSubmit}>
-                    <div className={styles.mailBox}>
-                        <div>
-                            <label>Imię:</label>
-                            <input type="text" value={this.state.name} onChange={e => this.setState({ name: e.target.value })}></input>
+                    <p className={styles.MailPar}>Proszę wypełnić formularz w celu rejestracji.</p>
+                    <form className={styles.Inputs} onSubmit={this.onSubmit}>
+                        <div className={styles.mailBox}>
+                            <div>
+                                <label>Imię:</label>
+                                <input type="text" value={this.state.name} onChange={e => this.setState({ name: e.target.value })}></input>
+                            </div>
+                            <div>
+                                <label>Email:</label>
+                                <input type="text" value={this.state.email} onChange={e => this.setState({ email: e.target.value })}></input>
+                            </div>
+                            <div>
+                                <label>Hasło: </label>
+                                <input type="password" value={this.state.password} onChange={e => this.setState({ password: e.target.value })}></input>
+                            </div>
+                            <div>
+                                <label>Powtórz Hasło: </label>
+                                <input type="password" value={this.state.password} onChange={e => this.setState({ password: e.target.value })}></input>
+                            </div>
                         </div>
-                        <div>
-                            <label>Email:</label>
-                            <input type="text" value={this.state.email} onChange={e => this.setState({ email: e.target.value })}></input>
-                        </div>
-                        <div>
-                            <label>Hasło: </label>
-                            <input type="password" value={this.state.password} onChange={e => this.setState({ password: e.target.value })}></input>
-                        </div>
-                        <div>
-                            <label>Powtórz Hasło: </label>
-                            <input type="password" value={this.state.password} onChange={e => this.setState({ password: e.target.value })}></input>
-                        </div>
+                    </form>
+                    <p className={styles.MailParBelow}>Tworząc u Nas konto zgadzasz sie na naszą <Link to="/privacy" className={styles.footerLinks} >Politykę prywatności</Link></p>
+
+                    <div className={styles.ButtonReg}>
+                        <button type="submit" onClick={this.onSubmit}>Zarejestruj się</button>
                     </div>
-                </form>
-                <p className={styles.MailParBelow}>Tworząc u Nas konto zgadzasz sie na naszą <Link to="/privacy" className={styles.footerLinks} >Politykę prywatności</Link></p>
-
-
-                <button type="submit" onClick={this.onSubmit}>Zarejestruj się</button>
                 </div>
                 {/* <RegisterSignIn></RegisterSignIn> */}
                 {this.state.err && <p className={styles.AllRegister} style={{ color: 'red' }}>{this.state.err}</p>}
