bd0c15c33b13423f01e519db3a4ffbe9752166c5
Adam Toczko <adamtoczko80@gmail.com>
2019-12-07 11:04:02 +0100

    dashboard further corrections

--- src/components/DashboardHeader.js
+++ src/components/DashboardHeader.js
@@ -14,8 +14,9 @@ class DashboardHeader extends React.Component {
         <div className={styles.header}>
         
         <p className={styles.header2}>Zobacz, ile żywności marnujesz  </p>
-        <Button color='youtube' as='a' target='blank' href='https://www.youtube.com/watch?v=IzznYgKs8Dw'>
-        <Icon name='youtube' /> YouTube
+        <Button 
+        color='youtube' as='a' target='blank' className={styles.headerButton}  href='https://www.youtube.com/watch?v=IzznYgKs8Dw'>
+        <Icon className={styles.headerButton} name='youtube' /> YouTube
         </Button>
         
         <p className={styles.header2}>Food Waste pokaże Tobie co możesz z tym zrobić  </p>
