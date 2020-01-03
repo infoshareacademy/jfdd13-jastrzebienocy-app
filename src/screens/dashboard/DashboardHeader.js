43b04c396b317fe43747d6ddfd55021d8650cb68
Kinga <zawarczynska.ko@gmail.com>
2020-01-03 23:27:29 +0100

    deleted repeated className in Button component used in DashboardHeaders

--- src/screens/dashboard/DashboardHeader.js
+++ src/screens/dashboard/DashboardHeader.js
@@ -9,7 +9,7 @@ class DashboardHeader extends React.Component {
         <div className={styles.header}>
         <p className={styles.header2}>Zobacz, ile żywności marnujesz  </p>
         <Button className={styles.headerButton} size='mini'
-        color='youtube' as='a' target='blank' className={styles.headerButton}  href='https://www.youtube.com/watch?v=IzznYgKs8Dw'>
+        color='youtube' as='a' target='blank'  href='https://www.youtube.com/watch?v=IzznYgKs8Dw'>
         <Icon className={styles.headerButton} name='youtube' /> YouTube
         </Button>
         <p className={styles.header2}>Food Waste pokaże Tobie co możesz z tym zrobić  </p>
